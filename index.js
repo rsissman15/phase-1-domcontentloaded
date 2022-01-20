eaf78229dbb1792957fed2b81eb5b86ae7b6a384
Rebecca Sissman <rsissman15@aol.com>
2022-01-20 11:37:09 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateParagrpah();
+  });
+
+  function updateParagraph(){
+      let p=document.getElementById('text').innerHTML = "This is really cool!";
+      return p;
+  }
+  
+  
